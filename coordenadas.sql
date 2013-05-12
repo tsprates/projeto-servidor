e8a934376a539d8acafcc5e14cecb92a8a8d98c0
Thiago Silva <tsprates@hotmail.com>
2013-05-12 13:36:07 -0300

    banco de dados

---
+++ coordenadas.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tempo de Geração: 
+-- Versão do Servidor: 5.5.24-log
+-- Versão do PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `projeto_monografia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `coordenadas`
+--
+
+CREATE TABLE IF NOT EXISTS `coordenadas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `longitude` varchar(500) NOT NULL,
+  `latitude` varchar(500) NOT NULL,
+  `telefone` varchar(500) NOT NULL,
+  `data_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
